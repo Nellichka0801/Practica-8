fcf5d0c1044d1712fa8bdfc142894505c1da5887
Nelli <nellichka0801@yandex.ru>
2022-11-29 07:41:43 +1200

    Arithmetic Mean Of Columns

---
+++ Task-54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Напишите программу, которая 
+////упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateArray(int rows, int columns)
+{
+    var random = new Random();
+    int[,] array = new int[rows, columns];
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] input) {
+    for (var i = 0; i < input.GetLength(0); i++)
+    {
+        for (var j = 0; j < input.GetLength(1); j++)
+        {
+            Console.Write(input[i, j]);
+            if (j != input.GetLength(1) - 1)
+            Console.Write(", ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrangeItemDescendingOrder(int[,] array)
+{
+    int temp = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(1); z++){
+                if (array[i, j] >= array[i, z])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, z];
+                    array[i, z] = temp;
+                } 
+            }
+        }
+    }
+    return array;
+}
+ 
+int[,] array = CreateArray(4, 4);
+PrintArray(array);
+Console.WriteLine("Новый массив");
+int[,] newArray = ArrangeItemDescendingOrder(array);
+PrintArray(newArray);
+
---
+++ Task-56/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей 
+//суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// наименьшей суммой элементов: 1 строка
+
+int[,] CreateArray(int rows, int columns)
+{
+    var random = new Random();
+    int[,] array = new int[rows, columns];
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+
+return array;
+}
+
+void PrintArray(int[,] input) {
+for (var i = 0; i < input.GetLength(0); i++)
+{
+for (var j = 0; j < input.GetLength(1); j++)
+{
+    Console.Write(input[i, j]);
+
+    if (j != input.GetLength(1) - 1)
+    Console.Write(", ");
+}
+Console.WriteLine();
+}
+}
+
+int ArithmeticMeanOfColumns (int[,] array)
+{
+    int stringMinSum = 0;
+    int sum = 0;
+    int  minSum = Int32.MaxValue;
+    
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        { 
+          sum = sum + array[i,j];
+        }
+        
+        if (minSum > sum) {
+            minSum = sum; stringMinSum = i+1;
+        } 
+        sum=0;
+        
+    }
+    return stringMinSum;
+}
+
+var array = CreateArray(4, 4);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine("Наименьшая сумма элементов в строке № " + ArithmeticMeanOfColumns (array));
+
+
